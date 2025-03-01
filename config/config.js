a6529669113146bca6192d993205315f4ca1e670
utkarshtripathi28 <utkarshtripathi0228@gmail.com>
2025-03-02 01:24:27 +0530

    changes

--- config/config.js
+++ config/config.js
@@ -5,25 +5,11 @@ let dotenv = require("dotenv").config({ path: fixture });
 let config = dotenv.parsed;
 module.exports = {
   local: {
-    username: process.env.DBUSERNAME
-      ? process.env.DBUSERNAME
-      : config?.DBUSERNAME,
-    password: process.env.DBPASSWORD
-      ? process.env.DBPASSWORD
-      : config?.DBPASSWORD,
+    username: process.env.DBUSERNAME ? process.env.DBUSERNAME : config?.DBUSERNAME,
+    password: process.env.DBPASSWORD ? process.env.DBPASSWORD : config?.DBPASSWORD,
     database: process.env.DBNAME ? process.env.DBNAME : config?.DBNAME,
     host: process.env.DBHOST ? process.env.DBHOST : config?.DBHOST,
     port: process.env.DBPORT ? process.env.DBPORT : config?.DBPORT,
-    dialect: process.env.DBDIALECT ? process.env.DBDIALECT : config?.DBDIALECT,
-    timezone: "+05:30",
-    dialectOptions: {
-      timezone: "+05:30",
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 60000,
-      idle: 10000,
-    },
+    dialect: 'postgres',
   }
 };
